74eac97c4cdeec4acb298dae86a55e2ecd03d34c
JEPTUI <alicejeptui@gail.com>
2023-11-08 02:34:11 +0000

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,8 +19,12 @@ class Auth():
         if excluded_paths is None or not excluded_paths:
             return True
         for excluded_path in excluded_paths:
-            if path.startswith(excluded_path.rstrip('/')):
-                return False
+            if excluded_path.endswith("*"):
+                prefix = excluded_path[:-1]
+                if path.startswith(prefix):
+                    return False
+                elif path == excluded_path:
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:

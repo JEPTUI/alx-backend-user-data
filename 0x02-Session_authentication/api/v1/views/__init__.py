588704539b394a9c9c71efa8aa90318c98908b48
JEPTUI <alicejeptui@gail.com>
2023-11-13 08:29:30 +0000

    New view for Session Authentication mandatory

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -7,5 +7,9 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
+from api.v1.views.session_auth import *
 
 User.load_from_file()
+app_views.add_url_rule(
+        '/auth_session/login', view_func=login, methods=['POST'],
+        strict_slashes=False)
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Creates a new flask view that handles
+all routes for the Session authentication
+"""
+
+from flask import Flask, jsonify, request, make_response
+from api.v1.views import app_views
+from api.v1.views.users import User
+from api.v1.app import auth
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """Handles all routes
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user[0].id)
+    user_data = user[0].to_json()
+
+    session_cookie_name = app.config.get('SESSION_NAME', '_my_session_id')
+    response = make_response(jsonify(user_data))
+    response.set_cookie(session_cookie_name, session_id)
+
+    return response

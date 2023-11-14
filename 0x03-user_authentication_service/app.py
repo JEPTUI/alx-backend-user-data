8b489ec2b15d8a6d7aa5d8fe119aa82c498a03b2
JEPTUI <alicejeptui@gail.com>
2023-11-14 11:31:55 +0000

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app
 """
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from auth import Auth
 
 
@@ -29,6 +29,23 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Implements the login function
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = make_response(
+                jsonify({"email": email, "message": "logged in"}))
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == '__main__':

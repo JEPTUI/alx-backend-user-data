a913cde0d1687ddb5fdd5133da7a82583e461b2a
JEPTUI <alicejeptui@gail.com>
2023-11-14 11:54:36 +0000

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,5 +77,18 @@ def profile():
         abort(403)
 
 
+@app.route('/get_reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Implements a function that resets password token
+    """
+    email = request.form.get('email')
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError as e:
+        abort(403, str(e))
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)

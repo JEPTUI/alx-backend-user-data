7b96b35271362983ade60f627c31538c0a6cf5e2
JEPTUI <alicejeptui@gail.com>
2023-11-14 11:41:20 +0000

    : Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,5 +48,19 @@ def login():
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.id)
+
+        return redirect('/')
+    else:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
